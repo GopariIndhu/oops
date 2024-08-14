149cf99b94f7c74fc0ffc04c84f8d4a59eb8519f
GopariIndhu <indhugopari@gmail.com>
2024-08-14 14:16:33 +0530

    String

---
+++ String/String.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		java.lang.String s1="java";
+		char ch[]= {'s','t','r','i','n','g','s'};
+		String s2=new String(ch);
+		String s3 =new String("example");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+	}
+
+}
---
+++ String/immutablestring.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class immutablestring {
+
+	public static void main(String[] args) {
+		//String s="Sachin";  
+		  // s=s.concat(" Tendulkar");  
+		   //System.out.println(s);  
+		String s="Sachin";  
+		s.concat(" Tendulkar");  
+		System.out.println(s);
+
+	}
+
+}
