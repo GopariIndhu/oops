e7be5b536ca8e2c5a4953ce5de80abdb08ab7f4c
GopariIndhu <indhugopari@gmail.com>
2024-08-25 20:08:31 +0530

    FunctionalInterface

---
+++ FunctionalInterface/FunctionalInterface.java
@@ -0,0 +1,29 @@
+@FunctionalInterface
+interface A 
+{
+    void show();
+    
+}
+
+// class B implements A 
+// {
+//     public void show()
+//     {
+//         System.out.println("in show");
+//     }
+// }
+
+public class FunctionalInterface
+{
+    public static void main(String args[])
+    {
+        A obj = new A()
+        {
+            public void show()
+            {
+                System.out.println("in show");
+            }
+        };
+        obj.show();
+    }
+}
